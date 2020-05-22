f05d0d23536fc6e51600d73efeade99b58b26049
Felipe Petersen <felipekpetersen@gmail.com>
2020-05-22 20:42:23 -0300

    setting up rootView with navigation

---
+++ Porting/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Porting
+//
+//  Created by Felipe Petersen on 22/05/20.
+//  Copyright © 2020 Paula Leite. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+
+    }
+
+}
